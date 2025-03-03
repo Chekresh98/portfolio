aa82d5eeb00cf815259fe1aa860d5739a441c3e1
H Chekresh <hosbectchekresh@gmail.com>
2025-03-03 17:35:33 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+    },
+    {
+        path: 'career',
+        loadComponent: () => import('./career/career.component').then(m => m.CareerComponent)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
