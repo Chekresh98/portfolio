37df8a7c8e1c59d30b67851ab60d0aff78b81fb5
H Chekresh <hosbectchekresh@gmail.com>
2025-04-11 19:46:34 +0530

    added fastapi

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  formata = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  }
 
+  constructor(private http: HttpClient) {}
+
+  submitForm() {
+    const params = new HttpParams()
+      .set('name', this.formata.name)
+      .set('email', this.formata.email)
+      .set('subject', this.formata.subject)
+      .set('message', this.formata.message);
+
+    this.http.post('http://127.0.0.1:8000/contact', params.toString())
+      .subscribe(response => {
+        console.log('Form submitted successfully', response);
+      });
+  }
+  
 }
